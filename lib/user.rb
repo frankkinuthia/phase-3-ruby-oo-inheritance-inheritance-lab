1ba85f5ab37eafee6b3db1f92a6286597aceb67d
Frank <f.kinuthia01@gmail.com>
2023-03-08 07:53:22 +0300

    Completed

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User
+    attr_reader :first_name, :last_name
+
+    def first_name=(first_name)
+        @first_name = first_name
+    end
+
+    def last_name=(last_name)
+        @last_name = last_name
+    end
+end
